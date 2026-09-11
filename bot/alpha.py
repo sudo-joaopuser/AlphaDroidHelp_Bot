sudo-joaopuser/AlphaDroidHelp_Bot

commit 55cfefcf055772546f5ed0c1d6fe2ca8e7570429

alpha: stop using '.upper()'
- xablocs: because its needless and the device code is case sensitive anyway! 😜

bot/alpha.py
@@ -38,7 +38,7 @@ def get_download_links(device_code):
             build_types.add(variant)
 
         message = (
-            f"✅ *Latest AlphaDroid for {device_code.upper()}:*\n\n"
+            f"✅ *Latest AlphaDroid for {device_code}:*\n\n"
             f"📱 Version: *{version}*\n"
             f"{status_icon} Status: *{status_text}*\n"
             f"🛠 Build Types: *{', '.join(build_types)}*\n"

commit c752aae4361a0be8fdbbb26b07e73ea69adeeb7c

alpha: stop using '.lower()'
Nothing's device names are capitalized, like Spacewar or Pong.
See Nothing Archive for more examples! [1]

[1]: https://nothingarchive.tech/docs/devices
Fixes: 55cfefcf0557 ("alpha: stop using '.upper()'")

bot/alpha.py
@@ -3,7 +3,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CallbackContext
 
 def get_download_links(device_code):
-    device_code = device_code.lower().strip()
+    device_code = device_code.strip()
     
     json_url = f"https://raw.githubusercontent.com/AlphaDroid-devices/OTA/alpha-16.1/{device_code}.json"
     changelog_url = f"https://github.com/AlphaDroid-devices/OTA/blob/alpha-16.1/changelog_{device_code}.txt"